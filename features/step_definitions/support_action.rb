56c3ff2efd991fcec6c53c107588872119eb5de7
Unknown <homersimpson213@icloud.com>
2019-02-01 14:49:24 +0400

    Добавелны сценарии
    негативная авторизация

--- features/step_definitions/support_action.rb
+++ features/step_definitions/support_action.rb
@@ -2,3 +2,16 @@
 When(/^Ждем "([^"]*)" секунд\(ы\)$/) do |second|
   sleep (second.to_i)
 end
+
+When(/^Ждем появления элемента "([^"]*)" с id "([^"]*)", но не более "([^"]*)" секунд$/) do |name, id, second|
+  $driver.set_implicit_wait(1)
+  spent_time = 0
+  until exist_element?(accessibility_id: id) do
+# sleep (0.5)
+    spent_time +=0.5
+    if spent_time > second.to_i
+      raise "Элемент #{name} не появился после #{spent_time} секунд ожидания!"
+    end
+  end
+  puts "Элемент #{name} появился спустя #{spent_time} секунд."
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -4,28 +4,30 @@ require 'cucumber'
 $project_path = ENV["IOS_PROJECT_PATH"]
 
 def caps1
-    {
-        caps: {
-            app: "#{$project_path}/1xBet.test.app",
-            platformName: "IOS",
-            platformVersion: "11.4",
-            deviceName: "321",
-            #xcodeOrgId: "6UWA23N6D3",
-            #xcodeSigningId: "iPhone Developer",
-            bundleId: "1xBet.1xBet",
-            #newCommandTimeout: "1000",
-            #automationName: "UIAutomation",
-            noReset:true,
-            unicodeKeyboard:true
-        }
-    }
+  {
+      caps: {
+          app: "/Users/sergeyignatov/QAIOSAuto/Mobile_iOS_QAAuto/1xBet.app",
+          platformName: "IOS",
+          platformVersion: "12.1",
+          deviceName: "iPhone XR",
+          #xcodeOrgId: "6UWA23N6D3",
+          #xcodeSigningId: "iPhone Developer",
+          bundleId: "1xBet.1xBet",
+          #newCommandTimeout: "1000",
+          #automationName: "UIAutomation",
+          noReset:true,
+          #automationName: "XCUITest",
+          useNewWDA: true,
+          unicodeKeyboard:true
+      }
+  }
 end
 
 def exist_element?(element)
-    $driver.set_implicit_wait(0.5)
-    begin
-        return true if find_element(element)
-    rescue
-        return false
-    end
+  $driver.set_implicit_wait(0.5)
+  begin
+    return true if find_element(element)
+  rescue
+    return false
+  end
 end
