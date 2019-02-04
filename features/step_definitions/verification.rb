1b71cfb7ce5e3301a7d161f98af75b77e0447edc
Unknown <homersimpson213@icloud.com>
2019-02-04 16:20:08 +0400

    Добавлен сценарий авторизации

--- features/step_definitions/verification.rb
+++ features/step_definitions/verification.rb
@@ -1,8 +1,70 @@
 
-When(/^Проверяем наличие элемента "([^"]*)" с name "([^"]*)"$/) do |name, id_name|
+When(/^Проверяем наличие элемента "([^"]*)" с id "([^"]*)"$/) do |name, id_name|
   if exist_element?(accessibility_id: id_name)
     puts ("Есть элемент #{name}.")
   else
     raise "Нет элемента #{name}!"
+  end
+end
+
+When(/^Проверяем, что в элементе "([^"]*)" с id "([^"]*)" есть текст$/) do |name, id|
+  if find_element(accessibility_id: id).text.empty?
+    raise("Текста нет!")
+  else
+    text = find_element(accessibility_id: id).value
+    puts("Для элемента #{name} задан текст #{text}")
+  end
+end
+
+When(/^Проверяем отсутствие элемента "([^"]*)" с id "([^"]*)"$/) do |name, id|
+  $driver.set_implicit_wait(1)
+  if exist_element?(accessibility_id: id)
+    raise "Есть элемент #{name}!"
+  else
+    puts ("Нет элемента #{name}.")
+  end
+end
+
+When(/^Проверяем, что в элементе "([^"]*)" с id "([^"]*)" нет текста$/) do |name, id|
+  if find_element(id: id).value.empty?
+    puts("Текста нет!")
+  else
+    text = find_element(accessibility_id: id).value
+    raise("Для элемента #{name} задан текст #{text}")
+  end
+end
+
+
+When(/^Проверяем, что значение всего текста элемента "([^"]*)" с id "([^"]*)" соответствует ранее записанному$/) do |name, id|
+  element = find_element(accessibility_id: id)
+  val_element = element.value
+
+  if val_element.empty?
+    raise ("Для элемента #{name} текст не задан!")
+  else
+    memory_file = File.new("#{$project_path}/reports/#{ENV["device"]}/state.txt")
+    val_memory = memory_file.read.chomp!
+    if val_memory == val_element
+      puts ("Значение #{val_memory} равно значению #{val_element}.")
+    else
+      raise ("Значение #{val_memory} НЕ равно значению #{val_element}!")
+    end
+  end
+end
+
+When(/^Проверяем, что значение всего текста элемента "([^"]*)" с id "([^"]*)" не соответствует ранее записанному$/) do |name, id|
+  element = find_element(accessibility_id: id)
+  val_element = element.value
+
+  if val_element.empty?
+    raise ("Для элемента #{name} текст не задан!")
+  else
+    memory_file = File.new("#{$project_path}/reports/#{ENV["device"]}/state.txt")
+    val_memory = memory_file.read.chomp!
+    if val_memory != val_element
+      puts ("Значение #{val_memory} НЕ равно значению #{val_element}.")
+    else
+      raise ("Значение #{val_memory}  равно значению #{val_element}!")
+    end
   end
 end
